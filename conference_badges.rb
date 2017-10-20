43d15e3f8653ec884503892f3e7f17df62950707
Eric McKinney <mckinney99@gmail.com>
2017-10-20 18:16:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+
+def batch_badge_creator(speakers)
+  speakers.each do |name|
+    index = speakers.index(name)
+    speakers[index] = "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.each do |name|
+    counter = 1
+    index = speakers.index(name)
+    room = index + 1
+    speakers[index] = "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(speakers)
+  speakers.each do |name|
+  index = speakers.index(name)
+  room = index + 1
+  puts "Hello, my name is #{name}."
+  puts "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+end
